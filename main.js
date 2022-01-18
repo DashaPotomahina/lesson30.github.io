82fd53e2db6d89ea8ed0fbcddd80748b4f88a998
DashaPotomahina <93083883+DashaPotomahina@users.noreply.github.com>
2022-01-18 14:19:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// let a, b
+// alert("давайте посчетаем периметр квадрата")
+// a=+prompt("введите сторону квадрата")
+// b=a*4
+// console.log ("P=", b)
+// alert ("P="+b)
+
+
+let a=document.querySelector('h1')
+let b=document.querySelector('button')
+a.style.color='black'
+function changeRed (){
+    a.style.color = 'red'
+}
+function changeYellow (){
+    a.style.color = 'yellow'
+}
+function changeGreen (){
+    a.style.color = 'green'
+}
+function changeBlue (){
+    a.style.color = 'blue'
+}
+function changeOrange (){
+    a.style.color = 'orange'
+}
